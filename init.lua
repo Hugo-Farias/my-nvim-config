0279c836ef6e876e73f883c66ee862235498dbb1
Hugo-Farias <hg80rod@gmail.com>
2025-05-20 07:21:28 -0300

    initial commit

---
+++ init.lua
@@ -0,0 +1,56 @@
+-- vim.g.mapleader = " "
+-- vim.g.maplocalleader = " "
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+require("keymaps")
+
+if vim.g.neovide then
+  vim.g.neovide_cursor_animation_length = 0
+  vim.g.neovide_cursor_trail_length = 0
+  vim.g.neovide_cursor_vfx_mode = ""
+end
+
+-- Change nvim's location to currently opened file's location
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    local file = vim.fn.expand("%:p")
+    if vim.fn.filereadable(file) == 1 then
+      vim.cmd("cd " .. vim.fn.fnamemodify(file, ":h"))
+    end
+  end,
+})
+
+-- vim.opt.clipboard = "unnamedplus"
+vim.opt.shiftwidth = 2
+
+vim.opt.scrolloff = 15
+
+-- Add lazy.nvim to runtime path
+local lazypath = "C:/Users/Hugo/AppData/Local/nvim-data/site/pack/lazy/start/lazy.nvim"
+vim.opt.rtp:prepend(lazypath)
+
+-- Install plugins
+require("lazy").setup({
+  { "tpope/vim-commentary" },
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  { "nvim-lua/plenary.nvim" },
+  { "nvim-telescope/telescope.nvim" },
+
+{ "ggandor/leap.nvim", lazy = false }
+--{
+--  "folke/flash.nvim",
+--  event = "VeryLazy",
+--  ---@type Flash.Config
+--  opts = {},
+--  -- stylua: ignore
+--  keys = {
+--    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+--    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+--    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+--    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+--    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+--  },
+--},
+})
---
+++ lua/keymaps.lua
@@ -0,0 +1,39 @@
+vim.keymap.set("n", "U", "<C-r>")
+
+vim.keymap.set("n", "<leader>w", ":w<CR>", { noremap = true, silent = true })
+-- gh: start of line (non-blank)
+vim.keymap.set({ "n", "v" }, "gh", "^", { desc = "Go to beginning of line (non-blank)" })
+
+-- gl: end of line (non-blank), overrides diagnostics float
+vim.keymap.set({ "n", "v" }, "gl", "g_", { desc = "Go to end of line (non-blank)" })
+
+-- Exit insert mode by double-tapping navigation keys
+vim.keymap.set("i", "jk", "<Esc>", { desc = "Exit insert mode with double tap 'h'" })
+-- vim.keymap.set("i", "jj", "<Esc>", { desc = "Exit insert mode with double tap 'j'" })
+-- vim.keymap.set("i", "kk", "<Esc>", { desc = "Exit insert mode with double tap 'k'" })
+vim.keymap.set("i", "kj", "<Esc>", { desc = "Exit insert mode with double tap 'l'" })
+
+vim.keymap.set("n", "<leader>ld", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
+
+vim.keymap.set("n", "<leader>tc", ":bd<CR>", { desc = "Close buffer" })
+
+vim.api.nvim_set_keymap("n", "<leader>f", ":Format<CR>", { noremap = true, silent = true })
+
+vim.keymap.set("n", "<C-=>", function()
+  vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1
+end)
+
+vim.keymap.set("n", "<C-->", function()
+  vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1
+end)
+
+-- Telescope
+vim.keymap.set("n", "<leader><leader>", "<cmd>Telescope find_files<cr>")
+vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>")
+vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
+vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")
+
+-- Remap Fowards and backwards
+-- nnoremap . ;
+-- nnoremap , ,
+
