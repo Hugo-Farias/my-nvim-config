0ac148ca6023abf9b690ae94b6bf6a08dffc41ff
Hugo-Farias <hg80rod@gmail.com>
2026-09-12 21:38:07 -0300

    ref: remove unused plugin files + references

--- lua/lazyload.lua
+++ lua/lazyload.lua
@@ -1,46 +1,46 @@
 require("lazy").setup({
-  -- { "nvim-lua/plenary.nvim" },
+  { "nvim-lua/plenary.nvim" },
 
   { require("lsp") },
   { require("plugins.mason") },
   { require("plugins.schemastore") },
   { require("plugins.treesitter") },
   { require("plugins.treesitter-context") },
-  -- { require("plugins.treesitter-textobjects") },
   { require("plugins.todo-comments") },
   { require("plugins.snacks") },
   { require("plugins.nvim-spider") },
-  -- { require("plugins.tide") },
   { require("plugins.harpoon") },
   -- { require("plugins.harpoon2") },
   { require("plugins.which-key") },
   { require("plugins.gitsigns") },
   { require("plugins.timber") },
-  -- { "Aasim-A/scrollEOF.nvim", event = { "CursorMoved", "WinScrolled" }, opts = {} },
-  -- { require("plugins.neoscroll") }, -- Smooth Scrolling
-  -- { require("plugins.render-markdown") },
   { "j-hui/fidget.nvim", version = "*", opts = {} },
-  -- { require("plugins.noice") },
-  -- { require("plugins.notify") },
 
-  ---- Buffer Navigation ----
+  ---- Language Support ----
+  { "kmonad/kmonad-vim" },
+  { "MeanderingProgrammer/render-markdown.nvim" },
+
+  ---- General Editing ----
+  { require("plugins.vim-camelsnek") },
+  { require("plugins.nvim-toggler") },
+  { require("plugins.nvim-ts-autotag") },
+  { require("plugins.multicursor") },
+  { require("plugins.rainbow-delimiters") },
+  { require("plugins.mini-surround") },
   { require("plugins.tabout") },
+  { require("plugins.mini-ai") },
+  { require("plugins.mini-splitjoin") },
+  { require("plugins.mini-align") },
+  -- { require("plugins.mini-pairs") },
 
   ---- Runners ----
   { require("plugins.jaq") },
 
   ---- File Explorer ----
   { require("plugins.yazi") },
-  -- { require("plugins.mini-files") },
-  -- { require("plugins.oil") },
-
-  ---- Multi Cursor ----
-  { require("plugins.multicursor") },
 
   ---- Formatter ----
   { require("plugins.conform") },
-  { require("plugins.nvim-ts-autotag") },
-  { require("plugins.rainbow-delimiters") },
 
   ---- AutoComplete ----
   -- { require("plugins.nvim-cmp") },
@@ -49,40 +49,17 @@ require("lazy").setup({
   ---- Snippets ----
   { require("plugins.luasnip") },
 
-  ---- AI assistant ----
+  ---- Assistant Agent ----
   { require("plugins.zbirenbaum-copilot") },
   -- { require("plugins.windsurf") },
-
-  ---- Mini Compilation ----
-  -- { require("plugins.mini-icons") },
-  { require("plugins.mini-surround") },
-  -- { require("plugins.mini-pairs") },
-  { require("plugins.mini-ai") },
-  { require("plugins.mini-splitjoin") },
-  { require("plugins.mini-align") },
 
   ---- Color Preview/Pickers ----
   { require("plugins.ccc") },
   { require("plugins.color-converter") },
 
-  ---- Buffer/Tab line ----
-  -- { require("plugins.bufferline") },
-  -- { require("plugins.mini-tabline") },
-
   ---- Status Line ----
-  -- { require("plugins.mini-statusline") },
   { require("plugins.lualine") },
-
-  ---- Language Support ----
-  { "kmonad/kmonad-vim" },
-
-  ---- General Editing ----
-  { require("plugins.vim-camelsnek") },
-  { require("plugins.nvim-toggler") },
 
   ---- Colors ----
   require("colors"),
-
-  ---- Temporary ----
-  -- { require("plugins.rustlings") },
 })
--- lua/plugins/nvim-toggler.lua
+++ lua/plugins/nvim-toggler.lua
@@ -1,20 +1,15 @@
 return {
   "nguyenvukhang/nvim-toggler",
-  lazy = false,
   opts = {
-
     -- inverses = {
     --   ["vim"] = "emacs",
     -- },
 
     -- removes the default <leader>i keymap
-    remove_default_keybinds = true,
+    remove_default_keybinds = false,
     -- removes the default set of inverses
     remove_default_inverses = false,
     -- auto-selects the longest match when there are multiple matches
     autoselect_longest_match = true,
-  },
-  keys = {
-    -- aasdfasdf,
   },
 }
