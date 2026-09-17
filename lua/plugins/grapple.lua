c749bfd55d97c9b151c3307855126d6505b19d1b
Hugo-Farias <hg80rod@gmail.com>
2026-09-17 08:38:10 -0300

    mod: grapple to attach keymap to name tag instead of index

--- lua/plugins/grapple.lua
+++ lua/plugins/grapple.lua
@@ -1,7 +1,3 @@
--- local set = vim.keymap.set
-
--- set("n", "<leader>G", require("grapple").toggle, { desc = "Grapple: Toggle Menu" })
-
 return {
   "cbochs/grapple.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons", lazy = true },
@@ -11,37 +7,38 @@ return {
     local grapple = require("grapple")
 
     local output = {
-      { "<leader>G", grapple.toggle_tags, desc = "Grapple: Toggle menu" },
-      {
-        "<leader>A",
-        function()
-          -- TODO: Must implement a way to prevent more than 10 files to be added
-          -- so index 11 is not overwritten
-          grapple.tag({ buffer = 0 })
-        end,
-        desc = "Grapple: Add buffer",
-      },
-      {
-        "<leader>M",
-        function()
-          grapple.tag({ buffer = 0, index = 11 })
-        end,
-        desc = "Grapple: Add buffer to 'Main'",
-      },
-      {
-        "qm",
-        function()
-          grapple.select({ index = 11 })
-        end,
-        desc = "Grapple: Switch to 'Main'",
-      },
+      { "qg", grapple.toggle_tags, desc = "Grapple: Toggle menu" },
+      -- {
+      --   "gA",
+      --   function()
+      --     grapple.tag({ buffer = 0 })
+      --   end,
+      --   desc = "Grapple: Add buffer",
+      -- },
     }
 
-    for i, key in ipairs({ "h", "j", "k", "l", ";", "y", "u", "i", "o", "p" }) do
+    local keymaps = { "h", "j", "k", "l", ";", "y", "u", "i", "o", "p", "m", "n", ",", "." }
+
+    for _, key in ipairs(keymaps) do
+      table.insert(output, {
+        "qa" .. key,
+        function()
+          grapple.tag({ name = key })
+          vim.notify(
+            "Added " .. vim.fn.expand("%:t") .. " to namespace: " .. key,
+            vim.log.levels.INFO,
+            { title = "Grapple" }
+          )
+        end,
+        desc = "Grapple: Add file to namespace: " .. key,
+      })
+    end
+
+    for _, key in ipairs(keymaps) do
       table.insert(output, {
         "q" .. key,
         function()
-          grapple.select({ index = i })
+          grapple.select({ name = key })
         end,
         desc = "which_key_ignore",
       })
