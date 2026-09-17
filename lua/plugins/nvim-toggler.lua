e05287767aac83469449ea5942711b4bab1d9d3a
Hugo-Farias <hg80rod@gmail.com>
2026-09-17 00:57:35 -0300

    add: nvim-toggler cursor now moves automatically to first occurence after the cursor

--- lua/plugins/nvim-toggler.lua
+++ lua/plugins/nvim-toggler.lua
@@ -6,10 +6,28 @@ return {
     -- },
 
     -- removes the default <leader>i keymap
-    remove_default_keybinds = false,
+    remove_default_keybinds = true,
     -- removes the default set of inverses
     remove_default_inverses = false,
     -- auto-selects the longest match when there are multiple matches
-    autoselect_longest_match = true,
+    autoselect_longest_match = false,
+  },
+  keys = {
+    {
+      "<leader>i",
+      function()
+        local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+        local line = vim.api.nvim_get_current_line()
+
+        local pos = vim.fn.match(line, [[\<\(true\|false\|on\|off\)\>]], col)
+
+        if pos >= 0 then
+          vim.api.nvim_win_set_cursor(0, { row, pos })
+        end
+
+        require("nvim-toggler").toggle()
+      end,
+      desc = "Flip boolean value",
+    },
   },
 }
